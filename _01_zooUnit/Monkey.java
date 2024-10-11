71c0389cd5dde5dea485b4479b5cecb81faaee67
WonjaeLee-L <dldnjswo@gmail.com>
2024-10-11 09:46:51 +0900

    상속

--- _01_zooUnit/Monkey.java
+++ _01_zooUnit/Monkey.java
@@ -2,9 +2,16 @@ package _01_zooUnit;
 
 public class Monkey extends SuperUnit {
 
-	@Override
+	@Override	// 선택적으로 재정의
 	public void feed() {
 		System.out.println("바나나 10개를 먹이로 준다!");
+	}
+
+	// superclass abstract시 add unimplemented methods 
+	@Override	// 필수적으로 재정의
+	public void kkk() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- _01_zooUnit/SuperUnit.java
+++ _01_zooUnit/SuperUnit.java
@@ -1,6 +1,7 @@
 package _01_zooUnit;
 
-public class SuperUnit {
+// 여기서 abstract -> sub에서 add unimplemented methods 
+public abstract class SuperUnit {	// 추상 클래스. 추상 메서드가 있다면 추상클래스, 특징은 객체를 만들 수 없다는 것.
 	public String zname = "human_zoo";
 	public String name = null;
 	public int age = 0;
@@ -9,6 +10,9 @@ public class SuperUnit {
 	public void feed() {
 		System.out.println("먹이주기");
 	}
+	
+	// 추상 메서드 만들면, 위에 클래스도 abstract
+	public abstract void kkk();		// 추상 메서드. 구현되지 않은 상태. 상속 받으면 반드시 override 해야 한다.
 
 	@Override
 	public String toString() {
